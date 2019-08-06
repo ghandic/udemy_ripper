b01ccd4d587f6b2b7f40cc86a38e87e2a77ee58d
Andy Challis <andy.challis@capgemini.com>
2019-08-06 17:37:25 +1000

    updated query

--- udemy.js
+++ udemy.js
@@ -1,9 +1,10 @@
 function getQuiz() {
 
-    class Answer {
+    class Option {
 
         constructor(node){
-            this.option = node.querySelector('div[class*=mc-quiz-answer--question-copy--]').innerText
+
+            this.description = node.querySelector('div[class*=mc-quiz-answer--question-copy--]').innerText
             this.correct = node.querySelector('div[class*=mc-quiz-answer--correct--]') != null
         }
     }
@@ -12,28 +13,47 @@ function getQuiz() {
     class Question {
 
         constructor(node){
-            this.question = node.querySelector('div[id=question-prompt]').innerText
             this.multiple = node.querySelector('input[type=radio]') === null
-            this.answers = [...node.querySelectorAll('li')].map(node => new Answer(node))
+            this.description = node.querySelector('div[id=question-prompt]').innerText
+            this.options = [...node.querySelector('ul[aria-labelledby=question-prompt]').querySelectorAll('li')].map(node => new Option(node))
 
             try {
               this.explanation = node.querySelector('div[class*=mc-quiz-question--explanation--]').querySelector('div').innerText
             } catch (error) {
-              this.explanation = null
+              this.explanation = ""
             }
 
         }
 
     }
 
-    questions = document.querySelectorAll('div[class*=detailed-result-panel--question-container]')
-    var quiz = [...questions].map(node => new Question(node))
+    class Quiz {
 
-    return quiz
+        constructor(node){
+            this.title = document.URL.split("/")[3]
+            this.problem_sets = [new ProblemSet(node)]
+        }
+
+    }
+
+    class ProblemSet {
+
+        constructor(node){
+            this.title = document.querySelector("h2[data-purpose=title]").innerText
+            this.intro = ""
+            this.questions = [...node.querySelectorAll('div[class*=detailed-result-panel--question-container]')].map(node => new Question(node))
+        }
+
+    }
+
+    return new Quiz(document.querySelector('body'))
+//    var quiz = [...questions].map(node => new Question(node))
+//
+//    return quiz
 }
 
 function downloadObjectAsJson(exportObj, exportName){
-    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj, null, 4));
     var downloadAnchorNode = document.createElement('a');
     downloadAnchorNode.setAttribute("href",     dataStr);
     downloadAnchorNode.setAttribute("download", exportName + ".json");
